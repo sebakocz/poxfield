c5a00c36ece971b4382426247aba58c39c66462a
Sebastian Koczwara <sebakocz@gmail.com>
2023-03-12 23:39:26 +0100

    initial main frame styling

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import MainFrame from '@src/components/MainFrame.vue'
 </script>
 
 <template>
-    <div>
-        <h1 class="text-3xl font-bold text-red-500 underline">Hello world!</h1>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-    </div>
-    <HelloWorld msg="Vite + Vue" />
+    <MainFrame />
 </template>
-
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    transition: filter 300ms;
-    will-change: filter;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/MainFrame.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex w-full gap-1">
+        <div v-for="index in 3" :key="index" class="w-full bg-amber-200 p-2">
+            Hey!
+        </div>
+    </div>
+    <div class="h-32 bg-amber-200">Main</div>
+</template>
