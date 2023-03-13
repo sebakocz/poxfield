7a5561efd5d42ba92e4cbafe0befba1c9aca77b7
Sebastian Koczwara <sebakocz@gmail.com>
2023-03-13 19:47:27 +0100

    fix server prody messing with prod

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/poxfield/',
     plugins: [vue()],
     server: {
-        proxy: {
-            '/api': {
-                target: 'https://www.poxnora.com',
-                changeOrigin: true,
-                secure: false,
-            },
-        },
+        proxy: isDev
+            ? {
+                  '/api': {
+                      target: 'https://www.poxnora.com',
+                      changeOrigin: true,
+                      secure: false,
+                  },
+              }
+            : undefined,
     },
     resolve: {
         alias: {
