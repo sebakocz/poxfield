26703483b31b78c82f24729cb9c28ecfe247e7ff
Sebastian Koczwara <sebakocz@gmail.com>
2023-03-20 20:14:01 +0100

    adjust searchbar placeholder length for mobile

--- src/components/RuneSearchBar.vue
+++ src/components/RuneSearchBar.vue
@@ -3,8 +3,12 @@
         <input
             type="text"
             class="w-full rounded p-2 text-center shadow lg:w-10/12"
-            placeholder="Search for names, classes, abilities and more..."
-            value="runesStore.searchQuery"
+            :placeholder="
+                isMobile
+                    ? 'Search...'
+                    : 'Search for names, classes, abilities and more...'
+            "
+            :value="runesStore.searchQuery"
             @input="onInput"
         />
     </div>
@@ -13,8 +17,10 @@
 <script setup lang="ts">
 import { useRunes } from '@src/stores/runesStore'
 import debounce from 'lodash/debounce'
+import { useMobileCheck } from '@src/composables/mobileCheck'
 
 const runesStore = useRunes()
+const { isMobile } = useMobileCheck()
 
 const onInput = debounce((event) => {
     runesStore.searchQuery = event.target.value
