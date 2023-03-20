e8e300a2d88c4c01cffd1e7d9ef62dec896dc88e
Sebastian Koczwara <sebakocz@gmail.com>
2023-03-20 20:01:53 +0100

    cleanup search bar debounec (thx Nils)

--- src/components/RuneSearchBar.vue
+++ src/components/RuneSearchBar.vue
@@ -4,27 +4,19 @@
             type="text"
             class="w-full rounded p-2 text-center shadow lg:w-10/12"
             placeholder="Search for names, classes, abilities and more..."
-            :value="searchQuery"
+            value="runesStore.searchQuery"
             @input="onInput"
         />
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
 import { useRunes } from '@src/stores/runesStore'
 import debounce from 'lodash/debounce'
 
 const runesStore = useRunes()
-const searchQuery = ref('')
 
 const onInput = debounce((event) => {
     runesStore.searchQuery = event.target.value
 }, 300)
-
-watchEffect(() => {
-    if (searchQuery.value !== runesStore.searchQuery) {
-        searchQuery.value = runesStore.searchQuery
-    }
-})
 </script>
